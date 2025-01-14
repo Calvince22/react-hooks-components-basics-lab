e09b96fc850a9def99930def44c52237d42e3183
Caleb Kiptoo <vincentcalebkip@gmail.com>
2025-01-14 18:04:49 +0300

    components

--- src/components/App.js
+++ src/components/App.js
@@ -16,13 +16,21 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+const About = () =>{
+  return(
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home />
+      <About />
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
     </div>
@@ -30,3 +38,4 @@ function App() {
 }
 
 export default App;
+
